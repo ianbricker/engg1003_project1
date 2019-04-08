02e4face56bbd315f26b6881bcf883ba341323f8
Ian Bricker <brickerian@gmail.com>
2019-04-08 08:41:09 +0000

    testing with strings

--- main.c
+++ main.c
@@ -2,15 +2,24 @@
 // week 6 lab commit
 int main() { 
    int key = 1; /* key is the amount of letter positions the rotation cipher will encrypt */
-   int x = 1;   /* x is the character of message text that will be encoded. If x=0, this corresponds to the letter a, while if x = 1, the letter b is represented */ 
+   int x = 0;   /* x is the character of message text that will be encoded. If x=0, this corresponds to the letter a, while if x = 1, the letter b is represented */ 
    int ex = 0; /* ex is the encrypted character of message text*/
    
-   char msg[255];
-   scanf("%s", msg);
+   /*char msg[255];
+   scanf("%s", msg);*/
    
-   ex = (x + key) % 26; /* encyrption equation */
+   char testString[6] = {'t', 'e', 's', 't', '\0'};
    
-   printf("%d\n", ex);
+   int count = 0;
+   for(count = 0; count < 5; count++) {
+       x = testString[count];
+       ex = (x + key) % 26;
+       printf("%d", ex);
+   }
+   
+   //ex = (x + key) % 26; /* encryption equation */
+   
+   //printf("%d\n", ex);
    
    
    
